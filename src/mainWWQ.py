aefcfd84d605db5b34c86f1a6b257250fc666426
rafaelleru <rafaelleru95103@gmail.com>
2018-10-30 18:34:23 +0100

    Adds size method to get the current size in the queue
    Adds docstrings
    
    Adds unit test

--- src/mainWWQ.py
+++ src/mainWWQ.py
@@ -1,7 +1,7 @@
 
 
 class WorkWaitQueue:
-    
+
     def __init__(self, name, nw):
         self._name = name
         self._nWorks = nw
@@ -9,19 +9,35 @@ class WorkWaitQueue:
     def name(self):
         return self._name
 
-    def nWorks(self):
+    def size(self):
         return self._nWorks
-        
+
+    def nWorks(self):
+        """
+        Returns the number of works waiting to be proccesseds.
+        """
+        return self._nWorks
+
     def addnWorks(self):
+        """
+        Increments the number of works waiting in the queue.
+        """
         self._nWorks += 1
 
     def delnWorks(self):
+        """
+        Decrements (if possible) the numbers of works waiting
+        in the queue
+        """
         if self._nWorks >= 1:
             self._nWorks -= 1
         else:
             print("La cola esta vacia")
-        
+
     def queueEmpty(self):
+        """
+        Returns wether the queue is empty or not.
+        """
         if self._nWorks == 0:
             return True
         else:
