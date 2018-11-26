d6428fe7beb8c9c44ffb1d10e2df3f57e21b4505
adriordi <adrihuelma22@gmail.com>
2018-11-26 15:17:50 +0100

    Fix api tests

--- src/test_WWQ.py
+++ src/test_WWQ.py
@@ -2,20 +2,9 @@ import unittest
 import json
 import requests
 from src.mainWWQ import WorkWaitQueue
+from requests import *
 
 url = "https://workwaitqueue.herokuapp.com/"
-
-def test_status(self):
-    response = requests.get(url)
-    self.assertEqual(self.response.status,200)
-
-def test_works(self):
-    response = requests.get(url+"works")
-    self.assertEqual(self.response.status,200)
-
-def test_empty(self):
-    response = requests.get(url+"empty")
-    self.assertEqual(self.response.status,200)
 
 class testWWQ(unittest.TestCase):
     def setUp(self):
@@ -26,26 +15,40 @@ class testWWQ(unittest.TestCase):
 
     def test_increment_works(self):
         # This avoid hardcode the number of jobs in the assert
-        previous_size = self.queue.size()
-        self.queue.addnWorks()
-        self.assertEqual(self.queue.size(), previous_size + 1)
+        previous_size = self.queue.nWorks()
+        self.queue.addWork()
+        self.assertEqual(self.queue.nWorks(), previous_size + 1)
 
     def test_decrement_works(self):
         # This avoid hardcode the number of jobs in the assert
-        previous_size = self.queue.size()
+        previous_size = self.queue.nWorks()
         final_size = 0 if previous_size is 0 else previous_size - 1
-        self.queue.delnWorks()
-        self.assertEqual(self.queue.size(), final_size)
+        self.queue.delWork()
+        self.assertEqual(self.queue.nWorks(), final_size)
 
     def test_decrement_works_non_null(self):
         # Add some works
-        self.queue.addnWorks()
-        self.queue.addnWorks()
+        self.queue.addWork()
+        self.queue.addWork()
         # This avoid hardcode the number of jobs in the assert
-        previous_size = self.queue.size()
+        previous_size = self.queue.nWorks()
         final_size = 0 if previous_size is 0 else previous_size - 1
-        self.queue.delnWorks()
-        self.assertEqual(self.queue.size(), final_size)
+        self.queue.delWork()
+        self.assertEqual(self.queue.nWorks(), final_size)
 
+    def test_status(self):
+        r = requests.get(url)
+        self.assertEqual(r.status_code,200,"Devuelve status 200")
+
+    def test_works(self):
+        r = requests.get(url+"works")
+        self.assertEqual(r.status_code,200,"Devuelve status 200")
+
+    def test_empty(self):
+        r = requests.get(url+"empty")
+        self.assertEqual(r.status_code,200,"Devuelve status 200")
+
+        
+        
 if __name__ == '__main__':
     unittest.main()
