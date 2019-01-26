5d1fc75861bbeaea1c54563eceb436db33cbe6eb
adriordi <adrihuelma22@gmail.com>
2019-01-26 18:46:52 +0100

    fabfile.py final version closes #33

--- despliegue/fabfile.py
+++ despliegue/fabfile.py
@@ -12,3 +12,13 @@ def Instalar():
 def Iniciar():
     "Inicia la aplicacion"
     run('cd proyectoIV/ && sudo gunicorn api_queue:__hug_wsgi__ -b 0.0.0.0:80')
+
+def Parar(): 
+    "Parar la ejecución matando el proceso"
+    run('sudo kill -9 $(ps -A -o pid,cmd | grep gunicorn | head -n 1 | cut -d " " -f 1)')
+
+def Actualizar():
+    "Reinicia la aplicacion y vuelve a ejecutarla"
+    Parar()
+    Instalar()
+    Iniciar()
